e165dcd4df2fd01863ee5dbce92ce87077a8c7bc
jfoody6 <164776412+jfoody6@users.noreply.github.com>
2024-04-17 02:56:05 +0000

    API Loaded

--- app.rb
+++ app.rb
@@ -4,13 +4,11 @@ require "sinatra"
 require "sinatra/reloader"
 require "http"
 
-EXCHANGE_RATE_KEY = ENV["EXCHANGE_RATE_API"]
+@EXCHANGE_RATE_KEY = ENV["EXCHANGE_RATE_API"]
 
 # define a route
 get("/") do
-
   
-
-
+  erb(:test)
   
 end
